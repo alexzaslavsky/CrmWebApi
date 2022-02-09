018d0fd314c44ea029b2e73d5b35e2ad22650ca8
EPAM\Oleksii_Zaslavskyi <oleksii_zaslavskyi@epam.com>
2022-02-09 17:28:50 +0200

    added GuardClauses2

---
+++ CrmWebApi.Helpers/GuardClauses2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CrmWebApi.Helpers
+{
+    public static class GuardClauses2
+    {
+        public static void IsNotNull(object argumentValue, string argumentName)
+        {
+            if (argumentValue == null)
+                throw new ArgumentNullException(argumentName);
+        }
+    }
+}
